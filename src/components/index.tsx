05bb83e2dc065354b7af92411f5e687079a7d743
cory-sydn <korysoydan@hotmail.com>
2023-02-21 11:28:22 +0300

    component index

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import Download from "./Download";
+import Features from "./Features";
+import SectionWrapper from "./SectionWrapper";
+import Button from "./Button";
+
+export {
+  Download,
+  Features,
+  SectionWrapper,
+  Button
+}
